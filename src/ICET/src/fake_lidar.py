037fe6290c2d79eb652d84dc573348c307e15474
Matt <mcdermatt@gmail.com>
2022-10-22 17:52:18 -0400

    got fake_lidar publishing pointcloud message that rviz can understand

--- src/ICET/src/fake_lidar.py
+++ src/ICET/src/fake_lidar.py
@@ -5,30 +5,80 @@ import numpy as np
 from rospy.numpy_msg import numpy_msg
 from rospy_tutorials.msg import Floats
 
+#test
+import std_msgs.msg as std_msgs
+import sensor_msgs
+from sensor_msgs import point_cloud2
+from sensor_msgs.msg import PointCloud2, PointField
+# import pcl
+# import sensor_msgs.point_cloud2 as pc2
+# import sensor_msgs#.msg as sensor_msgs
+
 """script to publish custom LIDAR point cloud messages"""
+
+
+def point_cloud(points, parent_frame):
+    """ Creates a point cloud message.
+    Args:
+        points: Nx7 array of xyz positions (m) and rgba colors (0..1)
+        parent_frame: frame in which the point cloud is defined
+    Returns:
+        sensor_msgs/PointCloud2 message
+    """
+    ros_dtype = PointField.FLOAT32
+    dtype = np.float32
+    itemsize = np.dtype(dtype).itemsize
+
+    data = points.astype(dtype).tobytes()
+
+    fields = [PointField(
+        name=n, offset=i*itemsize, datatype=ros_dtype, count=1)
+        for i, n in enumerate('xyzrgba')]
+
+    header = std_msgs.Header(frame_id=parent_frame, stamp=rospy.Time.now())
+
+    return PointCloud2(
+        header=header,
+        height=1,
+        width=points.shape[0],
+        is_dense=False,
+        is_bigendian=False,
+        fields=fields,
+        point_step=(itemsize * 3),
+        row_step=(itemsize * 3 * points.shape[0]),
+        data=data
+    )
 
 def main():
 
-    pcPub = rospy.Publisher('point_cloud', numpy_msg(Floats))
-    etcPub = rospy.Publisher('lidar_info', Num, queue_size=10) # Create a publisher that publishes String messages on a topic called "chatter". This publisher can hold 10 msgs that haven't been sent yet. 
+    # publish numpy array 
+    pcNpyPub = rospy.Publisher('numpy_cloud', numpy_msg(Floats), queue_size = 1)
+
+    # traditional pointcloud2 msg
+    pcPub = rospy.Publisher('point_cloud', PointCloud2, queue_size = 1)
+
+    # publish custom message with additional LIDAR info
+    etcPub = rospy.Publisher('lidar_info', Num, queue_size=10) #This publisher can hold 10 msgs that haven't been sent yet.  
     rospy.init_node('LidarScanner', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    r = 10
+    rate = rospy.Rate(r) # hz
     
     while not rospy.is_shutdown(): # While there's a roscore
-        
 
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         # use simulated point clouds I generated for a previous publication 
         # https://github.com/mcdermatt/ASAR/tree/main/v3/spherical_paper/MC_trajectories
         
         #use ROS timestamp as seed for scan idx
-        idx = int(rospy.get_time()%40) + 1
-        fn = "ASAR/v3/spherical_paper/MC_trajectories/scene1_scan" + str(idx) + ".txt"
-        pc = np.loadtxt(fn)
+        idx = int(r*rospy.get_time()%40) + 1
+        fn = "/home/derm/ASAR/v3/spherical_paper/MC_trajectories/scene1_scan" + str(idx) + ".txt"
+        pcNpy = np.loadtxt(fn)
 
         #publish point cloud as numpy_msg (rospy)
-        pcPub.publish(pc)
-        rospy.loginfo(pc)
+        pcNpyPub.publish(pcNpy)
+
+        #publish again as true point cloud message (so I can view with rviz)
+        pcPub.publish(point_cloud(pcNpy, 'map'))
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
@@ -47,7 +97,7 @@ def main():
         msg.status = status_str
 
         # Publish the string message and log it (optional)
-        rospy.loginfo(msg)
+        # rospy.loginfo(msg)
         etcPub.publish(msg)
         #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         
